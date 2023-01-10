3d82e37beece5f0a3b88a6dd7cfe1073dca61426
keishmen <57463087+FerEscobarDev@users.noreply.github.com>
2023-01-10 14:18:06 -0500

    Slider automático

--- src/pages/ServicesPage.vue
+++ src/pages/ServicesPage.vue
@@ -6,109 +6,135 @@
             </svg>
         </div>
         <div class="lg:max-w-[1654px] lg:max-h-[788px] w-full rounded-xl shadow-[0px_20px_30px_8px_rgba(0,0,0,0.3)]">
-            <div class="snap-x snap-mandatory overflow-x-scroll w-full inline-flex scrollbar-hide" ref="slider">
-                <CardSlide class="snap-center" :img="'../img/personal.png'" :title="'Desarrollo Personal'" :key="1">
-                    <h3 class="font-semibold text-xl text-brand-secundary mb-2">Talleres grupales</h3>
-                    <h3 class="font-semibold text-xl text-brand-secundary">Asesorías personalizadas</h3>
-                    <ul class="list-disc list-inside text-gray-600 font-semibold ml-2 mb-2">
-                        <li>¿Cómo potenciar tus habilidades?</li>
-                        <li>¿Cómo venderte de acuerdo con tu profesión?</li>
-                        <li>Vende tu servicio efectivamente</li>
-                        <li>Vence tus creencias limitantes</li>
-                        <li>Fortalecimiento de habilidades socioemocionales</li>
-                        <li>Acompañamiento en el proceso de definición</li>
-                        <li>Desarrollo personal, orientación profesional, aprendizaje de técnicas de estudio, acompañamiento para conseguir tus objetivos</li>
-                        <li>Mejora de la autoestima, desarrollo de habilidades sociales</li>
-                    </ul>
-                    <div class="flex justify-center items-center">
-                        <a 
-                            class="min-w-[150px] m-4 bg-brand-secundary p-2 text-white rounded-md border-brand-primary border-[1px]
-                                hover:bg-brand-claro hover:border-brand-secundary active:bg-brand-secundary active:border-brand-primary transition
-                                text-center" 
-                            href="https://api.whatsapp.com/send?phone=573213552207" 
-                            target="_blank">
-                            Hablemos
-                        </a> 
-                    </div>
-                </CardSlide>
-                <CardSlide class="snap-center" :img="'../img/organizacional.png'" :title="'Organizacional'" :key="2">
-                    <h3 class="font-semibold text-xl text-brand-secundary">Talleres y capacitaciones:</h3>
-                    <h5 class="font-semibold text-brand-claro ml-2 my-2">Servicio al cliente y comunicación asertiva</h5>
-                    <p class="text-justify text-gray-600 font-semibold ml-2">   
+            <div class="snap-x snap-mandatory overflow-x-scroll w-full inline-flex min-h-[788px] scrollbar-hide">
+                <Transition
+                    enter-active-class="transition-all ease-out"
+                    enter-from-class="transition-all opacity-80"
+                    enter-to-class="transition-all translate-x-32 opacity-100"
+                    leave-active-class="transition-all ease-in"
+                    leave-from-class="transition-all translate-x-32 opacity-100"
+                    leave-to-class="transition-all opacity-80"
+                >
+                    <CardSlide v-show="show === 1" class="snap-center" :img="'../img/personal.png'" :title="'Desarrollo Personal'">
+                        <h3 class="font-semibold text-xl text-brand-secundary mb-2">Talleres grupales</h3>
+                        <h3 class="font-semibold text-xl text-brand-secundary">Asesorías personalizadas</h3>
+                        <ul class="list-disc list-inside text-gray-600 font-semibold ml-2 mb-2">
+                            <li>¿Cómo potenciar tus habilidades?</li>
+                            <li>¿Cómo venderte de acuerdo con tu profesión?</li>
+                            <li>Vende tu servicio efectivamente</li>
+                            <li>Vence tus creencias limitantes</li>
+                            <li>Fortalecimiento de habilidades socioemocionales</li>
+                            <li>Acompañamiento en el proceso de definición</li>
+                            <li>Desarrollo personal, orientación profesional, aprendizaje de técnicas de estudio, acompañamiento para conseguir tus objetivos</li>
+                            <li>Mejora de la autoestima, desarrollo de habilidades sociales</li>
+                        </ul>
+                        <div class="flex justify-center items-center">
+                            <a 
+                                class="min-w-[150px] m-4 bg-brand-secundary p-2 text-white rounded-md border-brand-primary border-[1px]
+                                    hover:bg-brand-claro hover:border-brand-secundary active:bg-brand-secundary active:border-brand-primary transition
+                                    text-center" 
+                                href="https://api.whatsapp.com/send?phone=573213552207" 
+                                target="_blank">
+                                Hablemos
+                            </a> 
+                        </div>
+                    </CardSlide>
+                </Transition>
+                <Transition
+                    enter-active-class="transition-all ease-out"
+                    enter-from-class="transition-all opacity-80"
+                    enter-to-class="transition-all translate-x-32 opacity-100"
+                    leave-active-class="transition-all ease-in"
+                    leave-from-class="transition-all translate-x-32 opacity-100"
+                    leave-to-class="transition-all opacity-80"
+                >
+                    <CardSlide v-show="show === 2" class="snap-center" :img="'../img/organizacional.png'" :title="'Organizacional'">
+                        <h3 class="font-semibold text-xl text-brand-secundary">Talleres y capacitaciones:</h3>
+                        <h5 class="font-semibold text-brand-claro ml-2 my-2">Servicio al cliente y comunicación asertiva</h5>
+                        <p class="text-justify text-gray-600 font-semibold ml-2">   
 
-                        Los colaboradores de tu empresa recibirán acompañamiento para entender que es la relación que una 
-                        empresa establece con sus consumidores. Consiste en brindar todo el apoyo que el comprador de tus 
-                        productos o el contratista de tus servicios necesita para tener una buena experiencia en todas las 
-                        etapas de tu proceso de compra y postventa.
-                            
-                    </p>
-                    <h5 class="font-semibold text-brand-claro ml-2 my-2">Liderazgo y trabajo en equipo</h5>
-                    <p class="text-justify text-gray-600 font-semibold ml-2">   
+                            Los colaboradores de tu empresa recibirán acompañamiento para entender que es la relación que una 
+                            empresa establece con sus consumidores. Consiste en brindar todo el apoyo que el comprador de tus 
+                            productos o el contratista de tus servicios necesita para tener una buena experiencia en todas las 
+                            etapas de tu proceso de compra y postventa.
+                                
+                        </p>
+                        <h5 class="font-semibold text-brand-claro ml-2 my-2">Liderazgo y trabajo en equipo</h5>
+                        <p class="text-justify text-gray-600 font-semibold ml-2">   
 
-                            El liderazgo y trabajo en equipo se basan en una tendencia de equipos e influencia de grandes o pequeños 
-                            grupos en los que se necesita un líder para guiar a las personas y equipo hacia los objetivos que se marquen, 
-                            es por ello que para toda organización exitosa es necesario incluir capacitaciones en las que sus empleados 
-                            conozcan sobre el desarrollo de liderazgo.
-                    </p>
-                    <h3 class="font-semibold text-xl text-brand-secundary my-3">Selección y contratación de personal:</h3>
-                    <p class="text-justify text-gray-600 font-semibold">   
+                                El liderazgo y trabajo en equipo se basan en una tendencia de equipos e influencia de grandes o pequeños 
+                                grupos en los que se necesita un líder para guiar a las personas y equipo hacia los objetivos que se marquen, 
+                                es por ello que para toda organización exitosa es necesario incluir capacitaciones en las que sus empleados 
+                                conozcan sobre el desarrollo de liderazgo.
+                        </p>
+                        <h3 class="font-semibold text-xl text-brand-secundary my-3">Selección y contratación de personal:</h3>
+                        <p class="text-justify text-gray-600 font-semibold">   
 
-                        Una asesoría a tu área del manejo de personal que permita fortalecer las competencias y el plan de selección, 
-                        por medio de indicadores de gestión que apunten al objetivo deseado por la organización .
+                            Una asesoría a tu área del manejo de personal que permita fortalecer las competencias y el plan de selección, 
+                            por medio de indicadores de gestión que apunten al objetivo deseado por la organización .
 
-                    </p>
-                    <div class="flex justify-center items-center">
-                        <a 
-                            class="min-w-[150px] m-4 bg-brand-secundary p-2 text-white rounded-md border-brand-primary border-[1px]
-                                hover:bg-brand-claro hover:border-brand-secundary active:bg-brand-secundary active:border-brand-primary transition
-                                text-center" 
-                            href="https://api.whatsapp.com/send?phone=573213552207" 
-                            target="_blank">
-                            Hablemos
-                        </a> 
-                    </div>
-                </CardSlide>
+                        </p>
+                        <div class="flex justify-center items-center">
+                            <a 
+                                class="min-w-[150px] m-4 bg-brand-secundary p-2 text-white rounded-md border-brand-primary border-[1px]
+                                    hover:bg-brand-claro hover:border-brand-secundary active:bg-brand-secundary active:border-brand-primary transition
+                                    text-center" 
+                                href="https://api.whatsapp.com/send?phone=573213552207" 
+                                target="_blank">
+                                Hablemos
+                            </a> 
+                        </div>
+                    </CardSlide>
+                </Transition>
+                <Transition
+                    enter-active-class="transition-all ease-out"
+                    enter-from-class="transition-all opacity-80"
+                    enter-to-class="transition-all translate-x-32 opacity-100"
+                    leave-active-class="transition-all ease-in"
+                    leave-from-class="transition-all translate-x-32 opacity-100"
+                    leave-to-class="transition-all opacity-80"
+                >   
+                    <CardSlide v-show="show === 3" class="snap-center" :img="'../img/educativo.jpg'" :title="'Educativo'">
+                        <h3 class="font-semibold text-xl text-brand-secundary">Colegios:</h3>
+                        <h5 class="font-semibold text-brand-claro ml-2 my-2">Plan de orientación vocacional</h5>
+                        <p class="text-justify text-gray-600 font-semibold ml-2">   
 
-                <CardSlide class="snap-center" :img="'../img/educativo.jpg'" :title="'Educativo'" :key="3">
-                    <h3 class="font-semibold text-xl text-brand-secundary">Colegios:</h3>
-                    <h5 class="font-semibold text-brand-claro ml-2 my-2">Plan de orientación vocacional</h5>
-                    <p class="text-justify text-gray-600 font-semibold ml-2">   
-
-                        Proyectar el futuro profesional va mucho más allá de simplemente definir qué quieres estudiar. 
-                        Para este tipo de proyección necesitas identificar qué es lo que te gusta, reconocer tus fortalezas 
-                        y debilidades, y desarrollar hábitos que te lleven a la excelencia en cualquiera sea tu elección. 
-                        Con todo, sabemos que muchos de los consejos que se suelen recibir parecen ser muy abstractos, 
-                        dejándote con preguntas como: ¿por dónde empiezo? Es por esto que queremos compartirte cuatro tips 
-                        prácticos para ayudarte en la búsqueda de proyectar tu futuro profesional y cumplir tus metas.
-                            
-                    </p>
-                    <h3 class="font-semibold text-xl text-brand-secundary mt-2">Universidades:</h3>
-                    <h5 class="font-semibold text-brand-claro ml-2 my-2">Conferencias</h5>
-                    <ul class="text-gray-600 font-semibold list-none list-inside ml-2">
-                        <li>
-                            Destrezas altamente estimadas en el entorno laboral
-                        </li>
-                        <li>
-                            Hábitos de los graduados que trascienden
-                        </li>
-                        <li>
-                            ¿Como convertirte en un líder que sobresale?
-                        </li>
-                        <li>
-                            Claves para insistir y no desistir.
-                        </li>
-                    </ul>
-                    <div class="flex justify-center items-center">
-                        <a 
-                            class="min-w-[150px] m-4 bg-brand-secundary p-2 text-white rounded-md border-brand-primary border-[1px]
-                                hover:bg-brand-claro hover:border-brand-secundary active:bg-brand-secundary active:border-brand-primary transition
-                                text-center" 
-                            href="https://api.whatsapp.com/send?phone=573213552207" 
-                            target="_blank">
-                            Hablemos
-                        </a> 
-                    </div>
-                </CardSlide>
+                            Proyectar el futuro profesional va mucho más allá de simplemente definir qué quieres estudiar. 
+                            Para este tipo de proyección necesitas identificar qué es lo que te gusta, reconocer tus fortalezas 
+                            y debilidades, y desarrollar hábitos que te lleven a la excelencia en cualquiera sea tu elección. 
+                            Con todo, sabemos que muchos de los consejos que se suelen recibir parecen ser muy abstractos, 
+                            dejándote con preguntas como: ¿por dónde empiezo? Es por esto que queremos compartirte cuatro tips 
+                            prácticos para ayudarte en la búsqueda de proyectar tu futuro profesional y cumplir tus metas.
+                                
+                        </p>
+                        <h3 class="font-semibold text-xl text-brand-secundary mt-2">Universidades:</h3>
+                        <h5 class="font-semibold text-brand-claro ml-2 my-2">Conferencias</h5>
+                        <ul class="text-gray-600 font-semibold list-none list-inside ml-2">
+                            <li>
+                                Destrezas altamente estimadas en el entorno laboral
+                            </li>
+                            <li>
+                                Hábitos de los graduados que trascienden
+                            </li>
+                            <li>
+                                ¿Como convertirte en un líder que sobresale?
+                            </li>
+                            <li>
+                                Claves para insistir y no desistir.
+                            </li>
+                        </ul>
+                        <div class="flex justify-center items-center">
+                            <a 
+                                class="min-w-[150px] m-4 bg-brand-secundary p-2 text-white rounded-md border-brand-primary border-[1px]
+                                    hover:bg-brand-claro hover:border-brand-secundary active:bg-brand-secundary active:border-brand-primary transition
+                                    text-center" 
+                                href="https://api.whatsapp.com/send?phone=573213552207" 
+                                target="_blank">
+                                Hablemos
+                            </a> 
+                        </div>
+                    </CardSlide>
+                </Transition>
             </div>
         </div>
 
@@ -131,53 +157,38 @@ export default {
     },
 
     mounted () {
-        this.slider = this.$refs.slider;
+        this.playSlider()
     },
 
     data() {
         return {
-            slider: null,            
-            width: null,
-            left: 0,
-            right: 1,
+            show: 1,
         }
     },
 
     methods: {
         movSliderLeft() {
-            
-            this.width = this.slider.offsetWidth
 
-            this.slider.scrollTo({
-                left: this.width * this.left,
-                behaviior: 'smooth'
-            })
+            if(this.show === 1){
+                this.show = 3
+            }else{
+                this.show--
+            }
 
-            if(this.left > 0) {
-                this.left--
-                this.right--
-            }
-            else {
-                this.left = 0
-                this.right = 1
-            }
-            
         },
         movSliderRight() {
-            this.width = this.slider.offsetWidth
-            this.slider.scrollTo({
-                left: this.width * this.right,
-                behaviior: 'smooth'
-            })
-            if(this.right < this.slider.children.length - 1) {
-                this.right++
-                this.left++
+
+            if(this.show === 3){
+                this.show = 1
+            }else{
+                this.show++
             }
-            else {
-                this.right = this.slider.children.length - 1
-                this.left = this.right -1
-            }
-                        
+
+        },
+        playSlider() {
+
+            setInterval(this.movSliderRight, 5000)
+
         }
     },
 }
